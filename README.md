db971bc23c31663525f30c74db6df59009221d6b
MatheusNali <matheus.nali@usp.br>
2017-05-01 18:23:05 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-Projeto de Inteligência Artificial para a disciplina de Inteligência Artificial na USP.
-O projeto consiste na criação de um solucionador de Quebra-cabeças do tipo Sudoku. Esse tipo de quebra-cabeça tem como objetivo prencher números, de 1 a 9, em uma tabela de 9x9 a partir de poucos números iniciais em posições definidas de maneira que cumpram 3 requisitos:
-- Nenhum número repetido na mesma coluna
-- Nenhum número repetido na mesma linha
-- Nenhum número repetido em uma área quadrada de 3x3
-
-A solução desse puzzle se baseia no princípio de busca cega e busca ordenada.
+SudokuIA
--- SudokuSolver/src/sudokusolver/input.java
+++ SudokuSolver/src/sudokusolver/input.java
@@ -9,33 +9,29 @@ import java.io.FileReader;
  */
 public class input {
 
-    public static void main(String[] args) throws Exception {
-        readInput();
-    }
-	
-    private static int[][] readInput() throws Exception {
-        
+    private int[][] readInput() throws Exception {
+
         int[][] input = new int[9][9];
-        
+
         FileReader File = new FileReader("/home/az/Desktop/SudokuIA-Nali/SudokuInput.txt");
         BufferedReader Reader = new BufferedReader(File);
 
         String Line = Reader.readLine();
-        
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 input[i][j] = Integer.parseInt(Line.split(" ")[j]);
             }
             Line = Reader.readLine();
         }
-        
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
-                System.out.print(input[i][j]+" ");
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(input[i][j] + " ");
             }
             System.out.print("\n");
         }
-        
+
         return input;
 
     }
